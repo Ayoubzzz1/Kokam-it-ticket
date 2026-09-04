bf35f9558ee5d2de979ba2146b81c8e778665479
Ayoubzzz1 <kharroubiayoub27@gmail.com>
2026-09-04 07:48:52 +0100

    update seetings

--- Back/config/settings.py
+++ Back/config/settings.py
@@ -96,7 +96,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
-    "https://frontend-1-seven-sigma.vercel.app",
+    "https://kokam-it-ticket.vercel.app",
 ]
 
 FRONTEND_URL = os.getenv("FRONTEND_URL")
